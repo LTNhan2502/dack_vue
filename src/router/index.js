01b05993f26aeccaab1711da51fe493f0c60d5a6
LTNhan2502 <firstunio@gmail.com>
2024-03-24 20:58:54 +0700

    Create category view and add category function using json server to fetch API

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AddCategory from '../views/Category/AddCategory.vue'
+import Category from '../views/Category/Category.vue'
+
 
 const routes = [
   {
@@ -17,9 +19,14 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   },
   {
-    path: '/admin/addCategory',
+    path: '/admin/category/add',
     name: 'AddCategory',
     component: AddCategory
+  },
+  {
+    path: '/admin/category',
+    name: 'Category',
+    component: Category
   },
 ]
 
--- src/views/Category/AddCategory.vue
+++ src/views/Category/AddCategory.vue
@@ -7,24 +7,55 @@
                 </div>
             </div>
             <div class="row">
-                <div class="mb-3">
-                    <label for="" class="form-label">Name</label>
-                    <input
-                        type="text"
-                        name=""
-                        id=""
-                        class="form-control"
-                        placeholder=""
-                        aria-describedby="helpId"
-                    />
-                    <small id="helpId" class="text-muted">Help text</small>
-                </div>
-                
+                <form action="">
+                    <div class="mb-5">
+                        <label for="" class="form-label">Name</label>
+                        <input v-model="categoryName" type="text" name="" id="" class="form-control" placeholder=""
+                            aria-describedby="helpId" />
+                        <!-- <small id="helpId" class="text-muted">Help text</small> -->
+                    </div>
+                   <button type="button" class="btn btn-primary" @click="addCategory">Submit</button>
+                </form>
+
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    const axios = require("axios");
+    const sweetalert = require("sweetalert");
+    export default {
+        data(){
+            return {
+                id: "",
+                categoryName: "",
+            };
+        },
 
+        methods:{
+            addCategory(){
+                console.log(this.id, this.categoryName);
+                const newCate = {
+                    categoryName: this.categoryName,
+                };
+                const baseURL = "http://localhost:3000";
+                axios({
+                    method: 'post',
+                    url: `${baseURL}/category`,
+                    data: JSON.stringify(newCate),
+                    headers:{
+                        'Content-Type': 'application/json'
+                    } 
+                }).then(() => {
+                    sweetalert({
+                        text: "Added a category successfully",
+                        icon: "success",
+                    })
+                }).catch((err) => {
+                    console.log(err);
+                })
+            }
+        }
+    }
 </script>
