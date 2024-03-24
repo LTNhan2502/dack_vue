4583090a97ef3781371edb83dd243d83752313d6
LTNhan2502 <firstunio@gmail.com>
2024-03-24 15:47:40 +0700

    Update link css&js in index.html to use template

---
+++ src/views/Category/AddCategory.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="addCategory">
+        <div class="container">
+            <div class="row">
+                <div class="text-center">
+                    <h3>Add Category</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="mb-3">
+                    <label for="" class="form-label">Name</label>
+                    <input
+                        type="text"
+                        name=""
+                        id=""
+                        class="form-control"
+                        placeholder=""
+                        aria-describedby="helpId"
+                    />
+                    <small id="helpId" class="text-muted">Help text</small>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
